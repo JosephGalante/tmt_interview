e11a496b1bef1edbcde2befab5f63bec98b9b07e
Joe Galante <joe.galante@canopycapital.io>
2026-03-15 12:59:21 -0500

    implement full API

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,13 +1,36 @@
-from django.shortcuts import render
-from rest_framework import generics
+from django.db.models import Prefetch
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
 
-# Create your views here.
+
 class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+
+class OrdersOnTagView(APIView):
+    def get(self, request, id):
+        order_queryset = (
+            Order.objects.select_related(
+                "inventory",
+                "inventory__type",
+                "inventory__language",
+            )
+            .prefetch_related("inventory__tags", "tags")
+            .order_by("id")
+        )
+        tag = generics.get_object_or_404(
+            OrderTag.objects.prefetch_related(
+                Prefetch("orders", queryset=order_queryset)
+            ),
+            id=id,
+        )
+        serializer = OrderSerializer(tag.orders.all(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class OrderTagListCreateView(generics.ListCreateAPIView):
